1cce9e3c5b5046f5dd98602e52aa40858943457e
nguyenbahung94 <mr.hungcity@gmail.com>
2020-01-03 17:58:47 +0700

    update code

---
+++ app/src/main/java/com/example/collectiondecember2019/cloneReply/util/BindingAdapter.kt
@@ -0,0 +1,150 @@
+package com.example.collectiondecember2019.cloneReply.util
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowInsets
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.google.android.material.internal.ViewUtils.requestApplyInsetsWhenAttached
+
+
+@BindingAdapter("layoutFullscreen")
+fun View.bindLayoutFullscreen(previousFullscreen: Boolean, fullscreen: Boolean) {
+    if (previousFullscreen != fullscreen && fullscreen) {
+        systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
+}
+
+@BindingAdapter(
+    "paddingLeftSystemWindowInsets",
+    "paddingTopSystemWindowInsets",
+    "paddingRightSystemWindowInsets",
+    "paddingBottomSystemWindowInsets",
+    requireAll = false
+)
+fun View.applySystemWindowInsetsPadding(
+    previousApplyLeft: Boolean,
+    previousApplyTop: Boolean,
+    previousApplyRight: Boolean,
+    previousApplyBottom: Boolean,
+    applyLeft: Boolean,
+    applyTop: Boolean,
+    applyRight: Boolean,
+    applyBottom: Boolean
+) {
+    if (previousApplyLeft == applyLeft &&
+        previousApplyTop == applyTop &&
+        previousApplyRight == applyRight &&
+        previousApplyBottom == applyBottom
+    ) {
+        return
+    }
+
+    doOnApplyWindowInsets { view, insets, padding, _, _ ->
+        val left = if (applyLeft) insets.systemWindowInsetLeft else 0
+        val top = if (applyTop) insets.systemWindowInsetTop else 0
+        val right = if (applyRight) insets.systemWindowInsetRight else 0
+        val bottom = if (applyBottom) insets.systemWindowInsetBottom else 0
+
+        view.setPadding(
+            padding.left + left,
+            padding.top + top,
+            padding.right + right,
+            padding.bottom + bottom
+        )
+    }
+}
+
+fun View.doOnApplyWindowInsets(
+    block: (View, WindowInsets, InitialPadding, InitialMargin, Int) -> Unit
+) {
+    // Create a snapshot of the view's padding & margin states
+    val initialPadding = recordInitialPaddingForView(this)
+    val initialMargin = recordInitialMarginForView(this)
+    val initialHeight = recordInitialHeightForView(this)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding & margin states
+    setOnApplyWindowInsetsListener { v, insets ->
+        block(v, insets, initialPadding, initialMargin, initialHeight)
+        // Always return the insets, so that children can also use them
+        insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+}
+
+class InitialPadding(val left: Int, val top: Int, val right: Int, val bottom: Int)
+
+class InitialMargin(val left: Int, val top: Int, val right: Int, val bottom: Int)
+
+private fun recordInitialPaddingForView(view: View) = InitialPadding(
+    view.paddingLeft, view.paddingTop, view.paddingRight, view.paddingBottom
+)
+
+private fun recordInitialMarginForView(view: View): InitialMargin {
+    val lp = view.layoutParams as? ViewGroup.MarginLayoutParams
+        ?: throw IllegalArgumentException("Invalid view layout params")
+    return InitialMargin(lp.leftMargin, lp.topMargin, lp.rightMargin, lp.bottomMargin)
+}
+
+private fun recordInitialHeightForView(view: View): Int {
+    return view.layoutParams.height
+}
+
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        // We're already attached, just request as normal
+        requestApplyInsets()
+    } else {
+        // We're not attached to the hierarchy, add a listener to
+        // request when we are
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
+
+
+@BindingAdapter(
+    "glideSrc",
+    "glideCenterCrop",
+    "glideCircularCrop",
+    requireAll = false
+)
+fun ImageView.bindGlideSrc(
+    @DrawableRes drawableRes: Int?,
+    centerCrop: Boolean = false,
+    circularCrop: Boolean = false
+) {
+    if (drawableRes == null) return
+
+    createGlideRequest(
+        context,
+        drawableRes,
+        centerCrop,
+        circularCrop
+    ).into(this)
+}
+private fun createGlideRequest(
+    context: Context,
+    @DrawableRes src: Int,
+    centerCrop: Boolean,
+    circularCrop: Boolean
+): RequestBuilder<Drawable> {
+    val req = Glide.with(context).load(src)
+    if (centerCrop) req.centerCrop()
+    if (circularCrop) req.circleCrop()
+    return req
+}
