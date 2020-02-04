c4e762bccfe8020e0306d469325c90e2602ec303
nguyenbahung94 <mr.hungcity@gmail.com>
2020-02-04 09:31:34 +0700

    update

---
+++ app/src/main/java/com/example/collectiondecember2019/utils/Event.kt
@@ -0,0 +1,35 @@
+package com.example.collectiondecember2019.utils
+
+import androidx.lifecycle.Observer
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}
+
+//when to use : https://medium.com/better-programming/how-to-fix-a-serious-problem-in-livedata-android-594a3f18e981
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
+        }
+    }
+}
